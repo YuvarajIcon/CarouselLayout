7e6175b27c4730b9fc40c82f15f8b1531da9a97e
Yuvaraj <yuvaraj@ncompass.in>
2022-07-19 17:39:49 +0530

    init

--- CarouselLayout.podspec
+++ CarouselLayout.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'CarouselLayout'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of CarouselLayout.'
+  s.summary          = 'A collection view layout that emulates a Carousel.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -17,19 +17,17 @@ Pod::Spec.new do |s|
 #   * Write the description between the DESC delimiters below.
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
-  s.description      = <<-DESC
-TODO: Add long description of the pod here.
-                       DESC
+  s.description      = 'A UICompositional layout that emulates side scrolling carousel effect.'
 
-  s.homepage         = 'https://github.com/yuvaraj@ncompass.in/CarouselLayout'
+  s.homepage         = 'https://github.com/YuvarajIcon/CarouselLayout'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { 'yuvaraj@ncompass.in' => 'yuvaraj@ncompass.in' }
-  s.source           = { :git => 'https://github.com/yuvaraj@ncompass.in/CarouselLayout.git', :tag => s.version.to_s }
+  s.author           = { 'yuvaraj.australia@gmail.com' => 'YuvarajIcon' }
+  s.source           = { :git => 'https://github.com/YuvarajIcon/CarouselLayout.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '10.0'
-
+  s.ios.deployment_target = '13.0'
+  s.swift_versions = '4.0'
   s.source_files = 'CarouselLayout/Classes/**/*'
   
   # s.resource_bundles = {
---
+++ CarouselLayout/Classes/CarouselLayout.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  carousel
+//
+//  Created by Yuvaraj on 18/07/22.
+//
+
+import UIKit
+
+open class CarouselLayout: UICollectionViewCompositionalLayout {
+    fileprivate var scrollDirection: UICollectionView.ScrollDirection {
+        return configuration.scrollDirection
+    }
+    
+    @IBInspectable open var offFocusItemScale: CGFloat = 0.5
+    @IBInspectable open var offFocusItemAlpha: CGFloat = 0.5
+    @IBInspectable open var offFocusItemShift: CGFloat = 0
+    
+    override open func prepare() {
+        super.prepare()
+        self.setupCollectionView()
+    }
+    
+    override public init(section: NSCollectionLayoutSection, configuration: UICollectionViewCompositionalLayoutConfiguration) {
+        super.init(section: section, configuration: configuration)
+        self.setupCollectionView()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupCollectionView() {
+        guard let collectionView = self.collectionView else { return }
+        collectionView.decelerationRate = UIScrollViewDecelerationRateFast
+        if scrollDirection == .horizontal {
+            collectionView.contentInset = .init(top: 0, left: collectionView.bounds.width / 2, bottom: 0, right: collectionView.bounds.width / 2)
+        } else {
+            collectionView.contentInset = .init(top: collectionView.bounds.height / 2, left: 0, bottom: collectionView.bounds.height / 2, right: 0)
+        }
+    }
+    
+    override open func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+    override open func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let superAttributes = super.layoutAttributesForElements(in: rect),
+            let attributes = NSArray(array: superAttributes, copyItems: true) as? [UICollectionViewLayoutAttributes]
+            else { return nil }
+        return attributes.map({ self.transformLayoutAttributes($0) })
+    }
+    
+    fileprivate func transformLayoutAttributes(_ attributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        guard let collectionView = self.collectionView else { return attributes }
+        let isHorizontal = (scrollDirection == .horizontal)
+        
+        let collectionCenter = isHorizontal ? collectionView.center.x : collectionView.center.y
+        let offset = isHorizontal ? collectionView.contentOffset.x : collectionView.contentOffset.y
+        let normalizedCenter = (isHorizontal ? attributes.center.x : attributes.center.y) - offset
+        let maxDistance = (isHorizontal ? attributes.size.width : attributes.size.height)
+        let distance = min(abs(collectionCenter - normalizedCenter), maxDistance)
+        
+        let ratio = (maxDistance - distance) / maxDistance
+        let alpha = ratio * (1 - self.offFocusItemAlpha) + self.offFocusItemAlpha
+        let scale = ratio * (1 - self.offFocusItemScale) + self.offFocusItemScale
+        let shift = (1 - ratio) * self.offFocusItemShift
+        
+        //self explanatory, transparency value.
+        attributes.alpha = alpha
+        
+        //this will tell how to shrink/ expand/ rotate or do whatever you want to do to your cells.
+        attributes.transform3D = CATransform3DScale(CATransform3DIdentity, scale, scale, 1)
+        
+        if isHorizontal {
+            attributes.center.y = attributes.center.y + shift
+        } else {
+            attributes.center.x = attributes.center.x + shift
+        }
+        
+        return attributes
+    }
+    
+    //This tells the collectionview where to stop scrolling. i.e., to achieve a paging effect kinda stuffs.
+    override open func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        guard let collectionView = collectionView , !collectionView.isPagingEnabled,
+            let layoutAttributes = self.layoutAttributesForElements(in: collectionView.bounds)
+            else {
+            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
+        }
+        let isHorizontal = (scrollDirection == .horizontal)
+        let midSide = (isHorizontal ? collectionView.center.x : collectionView.center.y)
+        let proposedContentOffsetCenterOrigin = (isHorizontal ? proposedContentOffset.x : proposedContentOffset.y) + midSide
+        
+        var targetContentOffset: CGPoint
+        if isHorizontal {
+            let closest = layoutAttributes.sorted { abs($0.center.x - proposedContentOffsetCenterOrigin) < abs($1.center.x - proposedContentOffsetCenterOrigin) }.first!
+            targetContentOffset = CGPoint(x: floor(closest.center.x - midSide), y: proposedContentOffset.y)
+        }
+        else {
+            let closest = layoutAttributes.sorted { abs($0.center.y - proposedContentOffsetCenterOrigin) < abs($1.center.y - proposedContentOffsetCenterOrigin) }.first!
+            targetContentOffset = CGPoint(x: proposedContentOffset.x, y: floor(closest.center.y - midSide))
+        }
+        
+        return targetContentOffset
+    }
+}
+
